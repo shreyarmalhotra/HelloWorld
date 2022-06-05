7957f111af8b42e7472a032999678b64c278dd6e
shreyarmalhotra <shreyarakeshmalhotra@gmail.com>
2022-06-05 12:58:52 -0400

    Initial commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+
+        String pt="Hello";
+        System.out.println(pt);
+
+    }
+
+}
